ec55687d69b60e19cf28ec9388f9d29204eb9df3
galang <galangedilberto22@gmail.com>
2025-12-16 10:40:26 +0800

    first commit

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import "./About.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import profile_img from "../../assets/about_me_profile.svg";
+
+const About = () => {
+  return (
+    <div id="about" className="about">
+      <div className="about-title">
+        <h1>About Me</h1>
+        <img src={theme_pattern} alt="" />
+      </div>
+      <div className="about-sections">
+        <div className="about-left">
+          <img src={profile_img} alt="" />
+        </div>
+        <div className="about-right">
+          <div className="about-para">
+            <p>
+              I am an aspiring software and web developer with basic knowledge
+              of HTML, CSS, Java, PHP, and C# Windows Forms.
+            </p>
+            <p>
+              I have skills in data entry, teamwork, and collaboration. I am
+              eager to learn, improve my coding skills, and gain hands-on
+              experience in creating functional and user-friendly applications.
+            </p>
+          </div>
+          <div className="about-skills">
+            <div className="about-skill">
+              <p>HTML & CSS</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>REACT JS</p>
+              <hr style={{ width: "70%" }} />
+            </div>
+            <div className="about-skill">
+              <p>JAVASCRIPT</p>
+              <hr style={{ width: "50%" }} />
+            </div>
+            <div className="about-skill">
+              <p>TAILWIND</p>
+              <hr style={{ width: "60%" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="about-achievements">
+        <div className="about-achievement">
+          <h1>10+</h1>
+          <p>YEAR OF EXPERIENCE</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>90+</h1>
+          <p>PROJECTS COMPLETED</p>
+        </div>
+        <hr />
+        <div className="about-achievement">
+          <h1>15+</h1>
+          <p>HAPPY CLIENTS</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from "react";
+import Typed from "typed.js"; // import typed.js
+import "./Profile.css";
+import profile_img from "../../assets/Profile.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const Profile = () => {
+  useEffect(() => {
+    // initialize Typed.js
+    const typed = new Typed(".text", {
+      strings: ["Web Developer", "Junior Developer", "Frontend Developer"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 1000,
+      loop: true,
+    });
+
+    // destroy Typed instance during cleanup para iwas memory leak
+    return () => {
+      typed.destroy();
+    };
+  }, []);
+
+  return (
+    <div id="home" className="profile">
+      <img src={profile_img} alt="profile" />
+      <h1>
+        <span className="name">I'm Edilberto Galang,</span> And I'm <br />
+        <span id="text" className="text"></span>
+      </h1>
+      <p>
+        I'm a Fresh Graduate of Bachelor of Science in Information Systems,
+        skilled in (BASIC - HTML, CSS, JavaScript, React.) Passionate about
+        creating software and web applications, with strong teamwork and
+        collaboration skills.
+      </p>
+      <div className="profile-action">
+        <div className="profile-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with Me
+          </AnchorLink>
+        </div>
+        <div className="profile-resume">My Resume</div>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
